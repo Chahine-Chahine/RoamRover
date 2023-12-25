6adb6d0b53cc5d05f2aa9619a81de318e7f5dfc7
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-25 11:29:55 +0200

    [core]: implemented signup page and a route back to signin

--- Frontend/screens/SignupScreen.js
+++ Frontend/screens/SignupScreen.js
@@ -1,16 +1,95 @@
-import { View , Text} from "react-native";
-
-
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 
 const SignUpScreen = () => {
-    return(
-        <View>
-            <Text>
-                Hello from Signup!
-            </Text>
-        </View>
-    )
-}
+    const navigation = useNavigation();
+    const navigateSignIn = () => {
+        navigation.navigate("Signin");
+    };
 
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.header}>Sign Up</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Username"
+                    placeholderTextColor="#aaaaaa"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    placeholderTextColor="#aaaaaa"
+                    keyboardType="email-address"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry={true}
+                />
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+                <View style={styles.signupContainer}>
+                    <Text style={styles.signupText}>Already have an account? </Text>
+                    <TouchableOpacity onPress={navigateSignIn}>
+                        <Text style={styles.signupLink}>Sign In</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+    },
+    header: {
+        color: "#A78BFA",
+        fontSize: 34,
+        fontWeight: "bold",
+        marginBottom: 40,
+    },
+    input: {
+        height: 40,
+        borderBottomColor: "#aaaaaa",
+        borderBottomWidth: 1,
+        marginBottom: 20,
+        fontSize: 16,
+    },
+    buttonContainer: {
+        backgroundColor: "#A78BFA",
+        paddingVertical: 12,
+        paddingHorizontal: 120,
+        borderRadius: 10,
+        marginTop: 50,
+    },
+    buttonText: {
+        color: "#fff",
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    signupContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 20,
+    },
+    signupText: {
+        color: "#aaaaaa",
+        fontSize: 16,
+    },
+    signupLink: {
+        color: "#A78BFA",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+});
 
 export default SignUpScreen;
