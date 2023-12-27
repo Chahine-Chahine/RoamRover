548ddd27fbe3ec2c435797f6de3019d2296004e0
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-27 18:41:18 +0200

    [core]: implemented users migration

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'firstName',
+        'lastName',
     ];
 
     /**
