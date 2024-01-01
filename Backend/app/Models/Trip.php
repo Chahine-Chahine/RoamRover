d012adc15493f89f15d5b20c892a5796f44e04ab
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-01 15:38:58 +0200

    [core]: implemented Trip model with protected fillable

---
+++ Backend/app/Models/Trip.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'startingLocation',
+        'destinationLocation',
+        'totalBudget',
+        'receipt',
+        'locationID',
+        'roomID'
+    ];
+}
