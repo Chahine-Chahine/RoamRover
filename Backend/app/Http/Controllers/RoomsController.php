c13f7e175a2691cd1add512b46512ef649888fe4
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-01 14:24:39 +0200

    [core]: implemented createRoom function tested

--- Backend/app/Http/Controllers/RoomsController.php
+++ Backend/app/Http/Controllers/RoomsController.php
@@ -19,5 +19,23 @@ class RoomsController extends Controller
         return Room::findOrFail($id);
     }
 
+       // POST /rooms
+       public function createRoom(Request $request)
+       {
+           $validatedData = $request->validate([
+               'roomName' => 'required|string|max:255',
+               'creatorID' => 'required|integer|exists:users,id',
+               'participantsID' => 'required|array',
+               'participantsID.*' => 'integer|exists:users,id',
+           ]);
+   
+           $room = Room::create([
+               'roomName' => $validatedData['roomName'],
+               'creatorID' => $validatedData['creatorID'],
+               'participantsID' => json_encode($validatedData['participantsID']),
+           ]);
+   
+           return response()->json(['room' => $room, 'message' => 'Room created successfully'], 201);
+       }
    
 }
