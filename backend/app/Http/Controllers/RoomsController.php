89ea651aabe91a9618fe5ce13a10b7fb69a3c12f
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-05 23:05:38 +0200

    [backend][core]: implemented needed changes into the RoomsController tested

--- backend/app/Http/Controllers/RoomsController.php
+++ backend/app/Http/Controllers/RoomsController.php
@@ -25,14 +25,11 @@ class RoomsController extends Controller
            $validatedData = $request->validate([
                'roomName' => 'required|string|max:255',
                'creator_id' => 'required|integer|exists:users,id',
-               'participants_id' => 'required|array',
-               'participants_id.*' => 'integer|exists:users,id',
            ]);
    
            $room = Room::create([
                'roomName' => $validatedData['roomName'],
                'creator_id' => $validatedData['creator_id'],
-               'participants_id' => json_encode($validatedData['participants_id']),
            ]);
    
            return response()->json(['room' => $room, 'message' => 'Room created successfully'], 201);
