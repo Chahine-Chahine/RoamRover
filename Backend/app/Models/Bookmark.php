c4d66f8d07e041dee2092efb0538b5ce96334228
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-05 12:36:57 +0200

    [backend][core]: implemented modifications at Bookmark model

--- Backend/app/Models/Bookmark.php
+++ Backend/app/Models/Bookmark.php
@@ -9,17 +9,17 @@ class Bookmark extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'userID',
-        'locationID'
+        'user_id',
+        'location_id'
     ];
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'userID');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function location()
     {
-        return $this->belongsTo(Location::class, 'locationID');
+        return $this->belongsTo(Location::class, 'location_id');
     }
 }
