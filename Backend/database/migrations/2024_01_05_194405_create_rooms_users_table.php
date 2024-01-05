04bc63fdeb149bb99b8bc12c1d21adc0fc2c819a
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-05 21:52:31 +0200

    [backend][core]: implemented pivot table rooms_users

---
+++ Backend/database/migrations/2024_01_05_194405_create_rooms_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms_users', function (Blueprint $table) {
+            $table->unsignedBigInteger('room_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms_users');
+    }
+};
