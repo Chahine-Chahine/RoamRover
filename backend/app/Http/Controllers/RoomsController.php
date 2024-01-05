15bdd263f2245e2c495b03a99e1a75a8588213c1
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-05 23:07:15 +0200

    [backend][core]: implemented modifications at RoomsController functions to be compatable with pivot table

--- backend/app/Http/Controllers/RoomsController.php
+++ backend/app/Http/Controllers/RoomsController.php
@@ -4,35 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Room;
+use Illuminate\Support\Facades\DB; 
 
 class RoomsController extends Controller
 {
     // GET /rooms
     public function displayAllRooms()
     {
-        return Room::all();
+        return Room::with('users')->get();
     }
 
     // GET /rooms/{id}
     public function displayById($id)
     {
-        return Room::findOrFail($id);
+        return Room::with('users')->findOrFail($id);
     }
 
-       // POST /rooms
-       public function createRoom(Request $request)
-       {
-           $validatedData = $request->validate([
-               'roomName' => 'required|string|max:255',
-               'creator_id' => 'required|integer|exists:users,id',
-           ]);
-   
-           $room = Room::create([
-               'roomName' => $validatedData['roomName'],
-               'creator_id' => $validatedData['creator_id'],
-           ]);
-   
-           return response()->json(['room' => $room, 'message' => 'Room created successfully'], 201);
-       }
-   
+    // POST /rooms
+    public function createRoom(Request $request)
+    {
+        $validatedData = $request->validate([
+            'roomName' => 'required|string|max:255',
+            'creator_id' => 'required|integer|exists:users,id',
+            'participants' => 'required|array',
+            'participants.*' => 'integer|distinct|exists:users,id'
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            $room = Room::create([
+                'roomName' => $validatedData['roomName'],
+                'creator_id' => $validatedData['creator_id'],
+            ]);
+
+            $room->users()->attach(array_diff($validatedData['participants'], [$validatedData['creator_id']]));
+
+            // Commit the transaction
+            DB::commit();
+
+            return response()->json(['room' => $room, 'message' => 'Room created successfully'], 201);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
 }
--- backend/app/Models/Room.php
+++ backend/app/Models/Room.php
@@ -25,6 +25,7 @@ class Room extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class, 'rooms_users');
     }
+    
 }
