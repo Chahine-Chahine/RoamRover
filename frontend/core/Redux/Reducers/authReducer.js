b857d99d702ee4bce81d3c9d29468d76fc3dee7a
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-07 21:46:26 +0200

    [frontend][core]: implemented modifications to the authReducer

--- frontend/core/Redux/Reducers/authReducer.js
+++ frontend/core/Redux/Reducers/authReducer.js
@@ -2,43 +2,60 @@ const initialState = {
   isLoggedIn: false,
   user: null,
   token: null,
+  error: null,
 };
 
 function authReducer(state = initialState, action) {
   switch (action.type) {
-      case 'LOGIN_SUCCESS':
-          return {
-              ...state,
-              isLoggedIn: true,
-              user: action.payload.user,
-              token: action.payload.token,
-          };
-      case 'LOGIN_FAILURE':
-          return {
-              ...state,
-              isLoggedIn: false,
-              user: null,
-              token: null,
-              error: action.payload,
-          };
-          case 'REGISTER_SUCCESS':
-            return {
-              ...state,
-              isLoggedIn: true, 
-              user: action.payload,
-              token: null, 
-              error: null,
-            };
-          case 'REGISTER_FAILURE':
-            return {
-              ...state,
-              isLoggedIn: false,
-              user: null,
-              token: null,
-              error: action.payload,
-            };
-      default:
-          return state;
+    case 'LOGIN_SUCCESS':
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.payload.user,
+        token: action.payload.token,
+        error: null,
+      };
+    case 'LOGIN_FAILURE':
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+        token: null,
+        error: action.payload,
+      };
+    case 'REGISTER_SUCCESS':
+      return {
+        ...state,
+        isLoggedIn: true, 
+        user: action.payload,
+        token: null, 
+        error: null,
+      };
+    case 'REGISTER_FAILURE':
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+        token: null,
+        error: action.payload,
+      };
+    case 'LOGOUT':
+      return {
+        ...initialState,
+      };
+    case 'UPDATE_SUCCESS':
+      return {
+        ...state,
+        user: action.payload, 
+        error: null,
+      };
+    case 'UPDATE_FAILURE':
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
   }
 }
 
