3ffcfe245f176054eb396fad2ac324e7dede69f2
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-09 19:52:50 +0200

    [frontend][core]: added bookmarkReducer into the store reducer

--- frontend/core/Redux/store.js
+++ frontend/core/Redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from './Reducers/authReducer';
 import locationReducer from './Reducers/locationReducer';
+import bookmarkReducer from './Reducers/bookmarkReducer';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     locations: locationReducer,
+    bookmark: bookmarkReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
