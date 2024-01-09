814a67734c6e9ad4a92fc3cbb7dbb40705b5b1d4
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-10 00:52:37 +0200

    [frontend][core]: implemented changes in create and delete actions for bookmarks inorder to meet the backend endpoint

--- frontend/core/Redux/Actions/locationActions.js
+++ frontend/core/Redux/Actions/locationActions.js
@@ -4,7 +4,7 @@ import {
     FETCH_LOCATIONS_FAILURE
 } from './actionTypes';
 
-const baseUrl = 'http://192.168.43.29';
+const baseUrl = 'http://192.168.0.116';
 
 export const fetchLocations = () => {
     return async (dispatch) => {
