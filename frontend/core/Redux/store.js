98ca39beaf61ae4f1ab10666aafb65c6b25e344f
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-10 02:13:03 +0200

    [frontend][core]: implemented modifications to store to save state in local storage

--- frontend/core/Redux/store.js
+++ frontend/core/Redux/store.js
@@ -4,6 +4,27 @@ import authReducer from './Reducers/authReducer';
 import locationReducer from './Reducers/locationReducer';
 import bookmarkReducer from './Reducers/bookmarkReducer'; 
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch {
+    console.log('error during saving state')
+  }
+};
+
+const persistedState = loadState();
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -11,6 +32,11 @@ export const store = configureStore({
     bookmark: bookmarkReducer, 
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  preloadedState: persistedState,  
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export default store;
