43f328690fa70f6fae6255f940a21aacc071a802
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-11 19:45:01 +0200

    [frontend][core]: implemented lottieView into onboarding Screens

--- frontend/screens/OnboardingScreen.js
+++ frontend/screens/OnboardingScreen.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, Image, Touchable, TouchableOpacity } from "reac
 import React from "react";
 import Onboarding from 'react-native-onboarding-swiper';
 import { useNavigation } from "@react-navigation/native";
+import LottieView from 'lottie-react-native';
 
 
 export default OnboardingScreen = () => {
@@ -27,21 +28,36 @@ export default OnboardingScreen = () => {
         subTitleStyles = {{color: 'black'}}
         pages={[
             {
-            backgroundColor: '#A7F3D0',
-            image: <Image style={styles.OnboardingImage} source={require('../assets/Onboarding-1.png')} />,
+            backgroundColor: '#fff',
+            image:  <LottieView 
+            source={require('../assets/Animation - 1704993566447.json')} 
+            autoPlay 
+            loop 
+            style={styles.OnboardingImage}
+          /> ,
             title: 'Unlock Your Perfect Adventure',
             subtitle: ' Let us make planning a breeze! Use our Trip Budget Wizard to effortlessly generate a budget-friendly adventure',
             },
             {
-                backgroundColor: '#FEF3C7',
-                image: <Image style={styles.OnboardingImage} source={require('../assets/Onboarding-2.png')} />,
+                backgroundColor: '#fff',
+                image:  <LottieView 
+            source={require('../assets/Animation - 1704993589697.json')} 
+            autoPlay 
+            loop 
+            style={styles.OnboardingImage}
+          />,
                 title: 'Discover Your Destinations',
                 subtitle: ' Explore and verify each location effortlessly. Get a sneak peek before making decisions. Your journey is in your hands ',
                 },
             {
-                backgroundColor: '#A78BFA',
-                image: <Image style={styles.OnboardingImage} source={require('../assets/Onboarding-3.png')} />,
-                title: 'Share, Discuss, Decide - Together!',
+                backgroundColor: '#fff',
+                image:  <LottieView 
+                source={require('../assets/Animation - 1704993466799.json')} 
+                autoPlay 
+                loop 
+                style={styles.OnboardingImage}
+              />,
+                title: 'Share, Discuss, Decide',
                 subtitle: 'Trips are better with friends! Share plans, discuss, and take a vote on dates and activities. Modify your trip anytime, keeping everyone on board. ',
                 },
                 
@@ -58,8 +74,8 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
     },
     OnboardingImage: {
-        width: 300,
-        height: 300,
+        width: 200,
+        height: 200,
     }, 
     doneButton: {
         padding: 20,
