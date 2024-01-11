3f6425176dd3bee77aa5692bcd7310a14a456ab9
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-11 23:33:56 +0200

    [frontend][core]: implemented navigationReducer under test

---
+++ frontend/core/Redux/Reducers/navigationReducer.js
@@ -0,0 +1,20 @@
+// reducer.js
+import { SET_ACTIVE_TAB } from '../Actions/navigationAction';
+
+const initialState = {
+  activeTab: 'Explore', 
+};
+
+const navigationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ACTIVE_TAB:
+      return {
+        ...state,
+        activeTab: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default navigationReducer;
