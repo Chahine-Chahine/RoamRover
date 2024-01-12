da100232bcc5d01cb8b9e1020929169cf817602e
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-12 21:32:53 +0200

    [frontend][core]: implemented tripActions in Action Redux

---
+++ frontend/core/Redux/Actions/tripActions.js
@@ -0,0 +1,21 @@
+import {
+    FETCH_TRIPS_REQUEST,
+    FETCH_TRIPS_SUCCESS,
+    FETCH_TRIPS_FAILURE
+} from './actionTypes';
+
+const baseUrl = 'http://192.168.0.116';
+
+export const fetchLocations = () => {
+    return async (dispatch) => {
+        try {
+            dispatch({ type: FETCH_TRIPS_REQUEST });
+            const response = await fetch(`${baseUrl}:8000/api/trips`);
+            const data = await response.json();
+            dispatch({ type: FETCH_TRIPS_SUCCESS, payload: data });
+        } catch (error) {
+            dispatch({ type: FETCH_TRIPS_FAILURE, payload: error.message || 'An unknown error occurred'});
+            
+        }
+    };
+};
