810f14fa0bd86b5081cb91e67ff752dc528adcf0
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-12 23:23:23 +0200

    [frontend][core]: implemented modifications on tripReducer to the payload

--- frontend/core/Redux/Reducers/tripReducer.js
+++ frontend/core/Redux/Reducers/tripReducer.js
@@ -4,17 +4,18 @@ const initialState = {
     error: null,
 };
 
-const tripReducer = (state = initialState, action) => {
+function tripReducer(state = initialState, action) {
     switch (action.type) {
-        case 'FETCH_TRIPS_REQUEST':
-            return { ...state, loading: true };
-        case 'FETCH_TRIPS_SUCCESS':
-            return { ...state, loading: false, locations: action.payload };
-        case 'FETCH_TRIPS_FAILURE':
-            return { ...state, loading: false, error: action.payload };
-        default:
-            return state;
+      case 'FETCH_TRIPS_REQUEST':
+        return { ...state, loading: true };
+      case 'FETCH_TRIPS_SUCCESS':
+        return { ...state, loading: false, trips: action.payload.trips }; 
+      case 'FETCH_TRIPS_FAILURE':
+        return { ...state, loading: false, error: action.payload.error };
+      default:
+        return state;
     }
-};
+  }
+  
 
 export default tripReducer;
