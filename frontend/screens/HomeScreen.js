c386866a1be7c6f12536dbbab2cf0c38fe6de276
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-13 02:15:46 +0200

    [frontend][core]: implemented changes to description route to backend endpoint tested

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -34,7 +34,7 @@ const HomeScreen = () => {
                         key={trip.id.toString()}
                         onPress={() => navigateTripPage(trip)} 
                         title={trip.room?.room_name ?? 'Trip Room'}
-                        description={`${trip.room?.creator?.first_name ?? 'Description'} ${trip.room?.creator?.last_name ?? ''}`}
+                        description={`${trip.room?.room_description ?? 'Description'}`}
                         price={`$${trip.total_budget ?? ''} total`}
                         url={trip.locations[0]?.image ?? 'default_image_url'}
                         label={'Join'}
