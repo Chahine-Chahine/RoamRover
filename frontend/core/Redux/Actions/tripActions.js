fd463410bf1f598f880ec58c18e73513fc36e362
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-13 23:43:50 +0200

    [frontend][core]: implemented createTrip function in tripActions under test

--- frontend/core/Redux/Actions/tripActions.js
+++ frontend/core/Redux/Actions/tripActions.js
@@ -1,7 +1,10 @@
 import {
     FETCH_TRIPS_REQUEST,
     FETCH_TRIPS_SUCCESS,
-    FETCH_TRIPS_FAILURE
+    FETCH_TRIPS_FAILURE,
+    CREATE_TRIP_REQUEST,
+    CREATE_TRIP_SUCCESS,
+    CREATE_TRIP_FAILURE,
 } from './actionTypes';
 
 const baseUrl = 'http://192.168.0.116';
@@ -16,6 +19,26 @@ export const fetchTrips = () => {
         } catch (error) {
             dispatch({ type: FETCH_TRIPS_FAILURE, payload: error.message || 'An unknown error occurred'});
             
+        }
+    };
+};
+
+// createTrip action creator
+export const createTrip = (tripData) => {
+    return async (dispatch) => {
+        try {
+            dispatch({ type: CREATE_TRIP_REQUEST });
+            const response = await fetch(`${baseUrl}:8000/api/trips`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(tripData),
+            });
+            const data = await response.json();
+            dispatch({ type: CREATE_TRIP_SUCCESS, payload: data });
+        } catch (error) {
+            dispatch({ type: CREATE_TRIP_FAILURE, payload: error.message || 'An unknown error occurred' });
         }
     };
 };
