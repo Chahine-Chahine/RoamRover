615d433274906742132a53d89cc4daeeac1569bc
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-13 23:47:04 +0200

    [frontend][core]: implemented reducer cases in the tripReducer switch under test

--- frontend/core/Redux/Reducers/tripReducer.js
+++ frontend/core/Redux/Reducers/tripReducer.js
@@ -12,6 +12,12 @@ function tripReducer(state = initialState, action) {
         return { ...state, loading: false, trips: action.payload.trips }; 
       case 'FETCH_TRIPS_FAILURE':
         return { ...state, loading: false, error: action.payload.error };
+        case 'CREATE_TRIP_REQUEST':
+          return {...state, loading: true};
+      case 'CREATE_TRIP_SUCCESS':
+          return {...state, loading: false, trips: [...state.trips, action.payload]};
+      case 'CREATE_TRIP_FAILURE':
+          return {...state, loading: false, error: action.payload};
       default:
         return state;
     }
