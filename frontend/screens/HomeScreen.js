83661385db6dd26cfeb18119210edae29380e39f
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-15 02:05:30 +0200

    [frontend][bug-fix] : implemented useFocusEffect instead of useEffect hook tested

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTrips } from '../core/Redux/Actions/tripActions';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import Header from '../components/common/Header';
 import Card from '../components/common/Card';
 import NavigationBar from '../components/common/NavigationBar';
@@ -11,19 +11,22 @@ import LoadingScreen from './LoadingScreen';
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
-    const { trips, loading, error} = useSelector(state => state.trips);
+    const { trips, loading, error } = useSelector(state => state.trips);
     const navigation = useNavigation();
 
-    useEffect(() => {
-        dispatch(fetchTrips());
-    }, [dispatch]);
+    useFocusEffect(
+        React.useCallback(() => {
+            dispatch(fetchTrips());
+        }, [dispatch])
+    );
 
     const navigateTripPage = (trip) => {
         navigation.navigate('TripDetailsScreen', { trip });
     };
 
-    if (loading) return <LoadingScreen/>; 
+    if (loading) return <LoadingScreen />; 
     if (error) return <Text>Error: {error.message}</Text>;
+
     return (
         <View style={styles.container}>
             <Header />
@@ -38,7 +41,7 @@ const HomeScreen = () => {
                         price={`$${trip.total_budget ?? ''} total`}
                         url={trip.locations[0]?.image ?? 'default_image_url'}
                         label={'Join'}
-                        onAddPress={()=>{console.log('pressed')}}
+                        onAddPress={() => {console.log('pressed')}}
                         showBookmark={false}
                         onBookmarkPress={() => {}} 
                     />
