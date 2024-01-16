90d6215729f2a799cac04610f854ed0445c0fdcb
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-16 22:53:24 +0200

    [backend][core]: implemented questionnaires table and reference room_id as foreign key

---
+++ backend/database/migrations/2024_01_16_204435_create_questionnaires_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questionnaires', function (Blueprint $table) {
+            $table->id();
+            $table->string('type_question_response');
+            $table->string('time_question_response');
+            $table->integer('budget_question_response');
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questionnaires');
+    }
+};
