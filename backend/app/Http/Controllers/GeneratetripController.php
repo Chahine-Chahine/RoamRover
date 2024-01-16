3cc90d6105720b67d9937be44a1597c33e8b4488
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-16 21:44:03 +0200

    [backend][core]: implemented basic GeneratetripController

---
+++ backend/app/Http/Controllers/GeneratetripController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class GeneratetripController extends Controller
+{
+    public function generateTripAi(){
+       $auth_id = Auth::id();
+       
+
+
+
+    }
+}
