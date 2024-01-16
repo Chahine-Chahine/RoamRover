1d4f46a4c93d0b511f3f503df3f0255c7bbc6338
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-17 01:40:10 +0200

    [backend][core]: implemented basic prompt for AI with a dd function for testing

--- backend/app/Http/Controllers/GeneratetripController.php
+++ backend/app/Http/Controllers/GeneratetripController.php
@@ -1,16 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Questionnaire;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\Location;
+
 
 class GeneratetripController extends Controller
 {
     public function generateTripAi(){
-       $auth_id = Auth::id();
-       
-
-
-
-    }
+        $auth_id = Auth::id();
+        $location = Location::where("user_id", $auth_id)->first();
+        $Questionnaire = Questionnaire::where("user_id" , $auth_id)->first();
+        $type = $Questionnaire->type_question_response;
+        $time = $Questionnaire->time_question_response;
+        $budget = $Questionnaire->budget_question_response;
+     
+        $prompt = "Generate a trip according to $location so that: ";
+        $prompt .= "I am searching for $type trip, ";
+        $prompt .= "I am willing to spend $time in the trip so don't recommend exceeded time, ";
+        $prompt .= "I have $budget dollars budget for the trip";
+     
+        dd([
+            'auth_id' => $auth_id,
+            'location' => $location,
+            'questionnaire' => $Questionnaire,
+            'type' => $type,
+            'time' => $time,
+            'budget' => $budget,
+            'prompt' => $prompt
+        ]);
+     }
+     
 }
