179995346366c95a0469e4496a50f659f157f77b
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-19 01:39:50 +0200

    [frontend][core]: implemented create questionnaire reducer cases

---
+++ frontend/core/Redux/Reducers/generateaiReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+    Questionnaire: [],
+    loading: false,
+    error: null,
+};
+
+const generateaiReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'CREATE_QUESTIONNAIRE_REQUEST':
+            return { ...state, loading: true };
+        case 'CREATE_QUESTIONNAIRE_SUCCESS':
+            return { ...state, loading: false, Questionnaire: action.payload };
+        case 'CREATE_QUESTIONNAIRE_FAILURE':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export default generateaiReducer;
