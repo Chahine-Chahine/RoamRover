bbe128ce311f0be53bb048b81f0e15b12d344543
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-18 02:57:18 +0200

    [frontend][core]: implemented roomReducer

---
+++ frontend/core/Redux/Reducers/roomReducer.js
@@ -0,0 +1,61 @@
+import {
+    FETCH_CHATROOMS_REQUEST,
+    FETCH_CHATROOMS_SUCCESS,
+    FETCH_CHATROOMS_FAILURE,
+    CREATE_CHATROOMS_REQUEST,
+    CREATE_CHATROOMS_SUCCESS,
+    CREATE_CHATROOMS_FAILURE,
+} from '../Actions/actionTypes';
+
+const initialState = {
+    chatrooms: [],
+    loading: false,
+    error: null
+};
+
+const chatroomReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_CHATROOMS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case FETCH_CHATROOMS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                chatrooms: action.payload,
+                error: null
+            };
+        case FETCH_CHATROOMS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            };
+        case CREATE_CHATROOMS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case CREATE_CHATROOMS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                chatrooms: [...state.chatrooms, action.payload],
+                error: null
+            };
+        case CREATE_CHATROOMS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export default chatroomReducer;
