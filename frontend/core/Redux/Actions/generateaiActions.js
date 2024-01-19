8402947261189611b943dc1d603b4dce43d10bd4
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-19 04:08:53 +0200

    [frontend][core]: implemented fetchAIResponse action in generateaiActions

--- frontend/core/Redux/Actions/generateaiActions.js
+++ frontend/core/Redux/Actions/generateaiActions.js
@@ -1,23 +1,46 @@
-import axios from 'axios';
-import { baseUrl } from '../../helpers/baseUrl';
+import axios from "axios";
+import { baseUrl } from "../../helpers/baseUrl";
 import {
-    CREATE_QUESTIONNAIRE_REQUEST,
-    CREATE_QUESTIONNAIRE_SUCCESS,
-    CREATE_QUESTIONNAIRE_FAILURE
-} from './actionTypes';
+  CREATE_QUESTIONNAIRE_REQUEST,
+  CREATE_QUESTIONNAIRE_SUCCESS,
+  CREATE_QUESTIONNAIRE_FAILURE,
+  FETCH_AIRESPONSE_REQUEST,
+  FETCH_AIRESPONSE_SUCCESS,
+  FETCH_AIRESPONSE_FAILURE,
+} from "./actionTypes";
 
-export const createQuestionnaire = ({QuestionnaireData, token }) => {
-    return async (dispatch) => {
-        dispatch({ type: CREATE_QUESTIONNAIRE_REQUEST });
-        try {
-            const response = await axios.post(`${baseUrl}:8000/api/postQuestionnaire`,  QuestionnaireData, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            dispatch({ type: CREATE_QUESTIONNAIRE_SUCCESS, payload: response.data });
-        } catch (error) {
-            dispatch({ type: CREATE_QUESTIONNAIRE_FAILURE, payload: error });
+export const createQuestionnaire = ({ QuestionnaireData, token }) => {
+  return async (dispatch) => {
+    dispatch({ type: CREATE_QUESTIONNAIRE_REQUEST });
+    try {
+      const response = await axios.post(
+        `${baseUrl}:8000/api/postQuestionnaire`,
+        QuestionnaireData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-    };
+      );
+      dispatch({ type: CREATE_QUESTIONNAIRE_SUCCESS, payload: response.data });
+    } catch (error) {
+      dispatch({ type: CREATE_QUESTIONNAIRE_FAILURE, payload: error });
+    }
+  };
+};
+
+export const fetchAIResponse = () => {
+  return async (dispatch) => {
+    dispatch({ type: FETCH_AIRESPONSE_REQUEST });
+    try {
+      const response = await axios.post(`${baseUrl}:8000/api/generateTripAi`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      dispatch({ type: FETCH_AIRESPONSE_SUCCESS, payload: response.data });
+    } catch (error) {
+      dispatch({ type: FETCH_AIRESPONSE_FAILURE, payload: error });
+    }
+  };
 };
