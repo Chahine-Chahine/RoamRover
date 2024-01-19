2a348e6f47023015c7f2b7053b887d51b6fdf561
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-19 02:31:09 +0200

    [frontend][core]: implemented basic model with styles and extra useState hooks for Modal values

--- frontend/screens/AIquestionnaireScreen.js
+++ frontend/screens/AIquestionnaireScreen.js
@@ -24,9 +24,11 @@ const Questionnaire = () => {
   ];
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [tripType, setTripType] = useState('');
-  const [tripTime, setTripTime] = useState('');
-  const [budget, setBudget] = useState('');
+  const [ type_question_response, setTripType] = useState('');
+  const [ time_question_response, setTripTime] = useState('');
+  const [ budget_question_response, setBudget] = useState('');
+  const [room_name, setRoomName] = useState('');
+  const [room_description, setRoomDescription] = useState('');
 
   const dispatch = useDispatch();
   const navigation = useNavigation();
@@ -48,9 +50,9 @@ const Questionnaire = () => {
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
-      const finalBudget = parseFloat(budget); 
-      const finalAnswers = { tripType, tripTime, budget: finalBudget };
-      console.log(`Trip Type: ${tripType}, Trip Time: ${tripTime}, Budget: ${finalBudget}`); 
+      const finalBudget = parseFloat( budget_question_response); 
+      const finalAnswers = {  type_question_response,  time_question_response,  budget_question_response: finalBudget };
+      console.log(`Trip Type: ${ type_question_response}, Trip Time: ${ time_question_response}, Budget: ${finalBudget}`); 
       dispatch(createQuestionnaire(finalAnswers, token));
       navigateChat();
     }
@@ -58,42 +60,73 @@ const Questionnaire = () => {
   const currentQuestion = questions[currentQuestionIndex];
 
   return (
+    <><Modal
+    animationType="slide"
+    transparent={true}
+    visible={isModalVisible}
+    onRequestClose={() => setIsModalVisible(!isModalVisible)}
+  >
+    <View style={styles.modalView}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.modalTitle}>Create a Trip</Text>
+        <TextInput
+          placeholder="Room Name"
+          onChangeText={setRoomName}
+          value={room_name}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Room Description"
+          onChangeText={setRoomDescription}
+          value={room_description}
+          style={styles.input}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit}
+        >
+          <Text style={styles.buttonText}>Create Trip</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  </Modal>
     <View style={styles.container}>
       <Text style={styles.questionTitle}>{currentQuestion.question}</Text>
       {currentQuestion.answers && currentQuestion.answers.map((answer) => (
         <TouchableOpacity
-          key={answer}
-          style={[
+        key={answer}
+        style={[
             styles.answerButton,
-            currentQuestionIndex === 0 && tripType === answer && styles.selectedAnswer,
-            currentQuestionIndex === 1 && tripTime === answer && styles.selectedAnswer
+            currentQuestionIndex === 0 &&  type_question_response === answer && styles.selectedAnswer,
+            currentQuestionIndex === 1 &&  time_question_response === answer && styles.selectedAnswer
           ]}
           onPress={() => handleSelectAnswer(answer)}
-        >
+          >
           <Text 
             style={[
               styles.answerText,
-              currentQuestionIndex === 0 && tripType === answer && styles.selectedAnswerText,
-              currentQuestionIndex === 1 && tripTime === answer && styles.selectedAnswerText
+              currentQuestionIndex === 0 &&  type_question_response === answer && styles.selectedAnswerText,
+              currentQuestionIndex === 1 &&  time_question_response === answer && styles.selectedAnswerText
             ]}
-          >
+            >
             {answer}
           </Text>
         </TouchableOpacity>
       ))}
       {currentQuestion.input && (
         <TextInput
-          style={styles.input}
-          value={budget}
-          onChangeText={setBudget}
-          placeholder="Enter your budget"
-          keyboardType="numeric"
+        style={styles.input}
+        value={ budget_question_response}
+        onChangeText={setBudget}
+        placeholder="Enter your budget"
+        keyboardType="numeric"
         />
-      )}
+        )}
       <TouchableOpacity style={styles.nextButton} onPress={handleNextQuestion}>
         <Text style={styles.nextButtonText}>{currentQuestionIndex === questions.length - 1 ? "Done" : "Next"}</Text>
       </TouchableOpacity>
     </View>
+</>
   );
 };
 
@@ -155,7 +188,45 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     color: 'white',
-  }
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 120,
+    marginHorizontal: 10,
+    width: "95%",
+    height: "40%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    elevation: 5,
+  },
+  input: {
+    height: 60,
+    margin: 6,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderColor: "#A78BFA",
+  },
+  button: {
+    backgroundColor: "#A78BFA",
+    padding: 12,
+    borderRadius: 10,
+    alignItems: "center",
+    margin: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: 10,
+    marginVertical: 20,
+  },
 });
 
 export default Questionnaire;
