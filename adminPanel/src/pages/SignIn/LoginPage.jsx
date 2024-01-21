6b8d6815eb2ca1e989351f3602a4110b7981a660
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-21 04:02:14 +0200

    [adminPanel][core]: implemented right image and flex basic layout

--- adminPanel/src/pages/SignIn/LoginPage.jsx
+++ adminPanel/src/pages/SignIn/LoginPage.jsx
@@ -1,12 +1,14 @@
 import './LoginPage.css';
-
+import signInImage from '../../assets/Signin.png';
 
 const LoginPage = () =>{
   
   return(
     <div className="login-page">
       <div className="wrapper">
-        <div className="left-side"></div>
+        <div className="left-side">
+          <img className='image-signin' src={signInImage}/>
+        </div>
         <div className="right-side"></div>
       </div>
     </div>
