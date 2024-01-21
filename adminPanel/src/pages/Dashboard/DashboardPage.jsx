bacc690a69391cb563d2e700c9bc2dd94678d10b
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-21 05:16:53 +0200

    [adminPanel][styling]: added styling and image to the upper-left-card

--- adminPanel/src/pages/Dashboard/DashboardPage.jsx
+++ adminPanel/src/pages/Dashboard/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import './DashboardPage.css';
-
+import peopleImage from '../../assets/people.png';
 
 
 const DashboardPage = () => {
@@ -8,7 +8,10 @@ const DashboardPage = () => {
         <div className="dashboard-page">
             <div className="dashboard-wrapper">
                 <div className="left-dashboard">
-                    <div className="left-upper-card"></div>
+                    <div className="left-upper-card">
+                        <img src={peopleImage} className='dashboard-image'/>
+                        <h2 className='greeting-admin'>Hello, Chahine</h2>
+                    </div>
                     <div className="left-buttom-card"></div>
                 </div>
                 <div className="right-dashboard"></div>
