10af3a603bed0e13f0bed21a0b51758abbfe6a4c
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-22 06:16:28 +0200

    [adminPanel][core]: dispatched delete and edit location tested delete and a bug in edit

--- adminPanel/src/components/common/QuickActions.jsx
+++ adminPanel/src/components/common/QuickActions.jsx
@@ -5,7 +5,11 @@ import {
   deleteAnnouncement,
   updateAnnouncement,
 } from "../../core/redux/actions/announcementActions";
-import { createLocation } from "../../core/redux/actions/locationActions";
+import {
+  createLocation,
+  updateLocation,
+  deleteLocation,
+} from "../../core/redux/actions/locationActions";
 import "../../pages/Dashboard/DashboardPage.css";
 import Modal from "./Modal";
 
@@ -19,18 +23,23 @@ const QuickActions = () => {
   const [updatedAnnouncementBody, setUpdatedAnnouncementBody] = useState("");
   const [isCreateLocationModalOpen, setIsCreateLocationModalOpen] =
     useState(false);
-    const [locationData, setLocationData] = useState({
-      image: '',
-      description: '',
-      estimated_price: '',
-      title: '',
-      area: '',
-      rating: '',
-      coordinates: { latitude: '', longitude: '' },
-      est_time_spend: '',
-      tags: [],
-      category_ids: []
-    });
+  const [locationData, setLocationData] = useState({
+    image: "",
+    description: "",
+    estimated_price: "",
+    title: "",
+    area: "",
+    rating: "",
+    coordinates: { latitude: "", longitude: "" },
+    est_time_spend: "",
+    tags: "",
+    category_ids: [],
+  });
+  const [isDeletePlaceModalOpen, setIsDeletePlaceModalOpen] = useState(false);
+  const [isEditPlaceModalOpen, setIsEditPlaceModalOpen] = useState(false);
+  const [placeIdToDelete, setPlaceIdToDelete] = useState("");
+  const [placeIdToEdit, setPlaceIdToEdit] = useState("");
+
   const token = useSelector((state) => state.auth.token);
   const dispatch = useDispatch();
 
@@ -68,43 +77,53 @@ const QuickActions = () => {
   };
   const handleLocationInputChange = (e) => {
     const { name, value } = e.target;
-  
-    if (name.startsWith('coordinates.')) {
-      const coordField = name.split('.')[1]; 
-      setLocationData(prevState => ({
+    if (name === "category_ids") {
+      setLocationData({ ...locationData, [name]: [...value] });
+    } else if (name.startsWith("coordinates.")) {
+      const coordField = name.split(".")[1];
+      setLocationData((prevState) => ({
         ...prevState,
         coordinates: {
           ...prevState.coordinates,
-          [coordField]: value
-        }
+          [coordField]: value,
+        },
       }));
     } else {
-      setLocationData(prevState => ({
-        ...prevState,
-        [name]: value
-      }));
+      setLocationData({ ...locationData, [name]: value });
     }
   };
-  // Handle creating a location
-  const handleCreateLocation = async(e) => {
+
+  const handleCreateLocation = (e) => {
     e.preventDefault();
-  
     const payload = {
       ...locationData,
+      id: Number(locationData.id),
       estimated_price: Number(locationData.estimated_price),
-      category_ids: [locationData.category_ids].filter(Boolean),
-      tags: locationData.tags.split(',').map(tag => tag.trim()),
+      category_ids: locationData.category_ids.map(Number),
+      tags: locationData.tags.split(",").map((tag) => tag.trim()),
       latitude: Number(locationData.coordinates.latitude),
-      longitude: Number(locationData.coordinates.longitude)
+      longitude: Number(locationData.coordinates.longitude),
     };
-  
-  await  dispatch(createLocation(payload, token));
+    dispatch(createLocation(payload, token));
     setIsCreateLocationModalOpen(false);
-  };  
+  };
+
+  const handleDeleteLocation = (e) => {
+    e.preventDefault();
+    dispatch(deleteLocation(placeIdToDelete, token));
+    setIsDeletePlaceModalOpen(false);
+  };
+
+  const handleEditLocation = (e) => {
+    e.preventDefault();
+    dispatch(updateLocation(placeIdToEdit, locationData, token));
+    setIsEditPlaceModalOpen(false);
+  };
 
   return (
     <div className="quick-actions">
       <h2>Quick Actions</h2>
+
       <div className="section">
         <h3>Announcements</h3>
         <div className="actions">
@@ -128,6 +147,7 @@ const QuickActions = () => {
           </button>
         </div>
       </div>
+
       <div className="section">
         <h3>Locations</h3>
         <div className="actions">
@@ -135,10 +155,20 @@ const QuickActions = () => {
             className="action-button green"
             onClick={() => setIsCreateLocationModalOpen(true)}
           >
-            Add a location
+            Add a Location
           </button>
-          <button className="action-button red">Delete a location</button>
-          <button className="action-button yellow">Edit a location</button>
+          <button
+            className="action-button red"
+            onClick={() => setIsDeletePlaceModalOpen(true)}
+          >
+            Delete a Location
+          </button>
+          <button
+            className="action-button yellow"
+            onClick={() => setIsEditPlaceModalOpen(true)}
+          >
+            Edit a Location
+          </button>
         </div>
       </div>
 
@@ -201,96 +231,216 @@ const QuickActions = () => {
       </Modal>
 
       {/* Modal for Creating a Location */}
-      <Modal isOpen={isCreateLocationModalOpen} onClose={() => setIsCreateLocationModalOpen(false)} title="Add a Place">
-  <form onSubmit={handleCreateLocation}>
-    <input
-      type="text"
-      name="title"
-      value={locationData.title}
-      onChange={handleLocationInputChange}
-      placeholder="Title"
-    />
-    <input
-      type="text"
-      name="description"
-      value={locationData.description}
-      onChange={handleLocationInputChange}
-      placeholder="Description"
-    />
-    <input
-      type="text"
-      name="image"
-      value={locationData.image}
-      onChange={handleLocationInputChange}
-      placeholder="Image URL"
-    />
-    <input
-      type="number"
-      name="estimated_price"
-      value={locationData.estimated_price}
-      onChange={handleLocationInputChange}
-      placeholder="Estimated Price"
-    />
-    <input
-      type="text"
-      name="area"
-      value={locationData.area}
-      onChange={handleLocationInputChange}
-      placeholder="Area"
-    />
-    <input
-      type="number"
-      name="rating"
-      value={locationData.rating}
-      onChange={handleLocationInputChange}
-      placeholder="Rating"
-    />
-   <input
-  type="number"
-  name="coordinates.latitude"
-  value={locationData.coordinates.latitude}
-  onChange={handleLocationInputChange}
-  placeholder="Latitude"
-/>
-<input
-  type="number"
-  name="coordinates.longitude"
-  value={locationData.coordinates.longitude}
-  onChange={handleLocationInputChange}
-  placeholder="Longitude"
-/>
-    <input
-      type="number"
-      name="est_time_spend"
-      value={locationData.est_time_spend}
-      onChange={handleLocationInputChange}
-      placeholder="Estimated Time to Spend"
-    />
-    <select
-      name="category_ids"
-      value={locationData.category_ids}
-      onChange={handleLocationInputChange}
-      multiple
-    >
-      <option value="1">Beach</option>
-      <option value="2">Hiking</option>
-      <option value="3">Restaurants</option>
-      <option value="4">Ruins</option>
-    </select>
+      <Modal
+        isOpen={isCreateLocationModalOpen}
+        onClose={() => setIsCreateLocationModalOpen(false)}
+        title="Add a Place"
+      >
+        <form onSubmit={handleCreateLocation}>
+          <input
+            type="text"
+            name="title"
+            value={locationData.title}
+            onChange={handleLocationInputChange}
+            placeholder="Title"
+          />
+          <input
+            type="text"
+            name="description"
+            value={locationData.description}
+            onChange={handleLocationInputChange}
+            placeholder="Description"
+          />
+          <input
+            type="text"
+            name="image"
+            value={locationData.image}
+            onChange={handleLocationInputChange}
+            placeholder="Image URL"
+          />
+          <input
+            type="number"
+            name="estimated_price"
+            value={locationData.estimated_price}
+            onChange={handleLocationInputChange}
+            placeholder="Estimated Price"
+          />
+          <input
+            type="text"
+            name="area"
+            value={locationData.area}
+            onChange={handleLocationInputChange}
+            placeholder="Area"
+          />
+          <input
+            type="number"
+            name="rating"
+            value={locationData.rating}
+            onChange={handleLocationInputChange}
+            placeholder="Rating"
+          />
+          <input
+            type="number"
+            name="coordinates.latitude"
+            value={locationData.coordinates.latitude}
+            onChange={handleLocationInputChange}
+            placeholder="Latitude"
+          />
+          <input
+            type="number"
+            name="coordinates.longitude"
+            value={locationData.coordinates.longitude}
+            onChange={handleLocationInputChange}
+            placeholder="Longitude"
+          />
+          <input
+            type="number"
+            name="est_time_spend"
+            value={locationData.est_time_spend}
+            onChange={handleLocationInputChange}
+            placeholder="Estimated Time to Spend"
+          />
+          <select
+            name="category_ids"
+            value={locationData.category_ids}
+            onChange={handleLocationInputChange}
+            multiple
+          >
+            <option value="1">Beach</option>
+            <option value="2">Hiking</option>
+            <option value="3">Restaurants</option>
+            <option value="4">Ruins</option>
+          </select>
 
-    {/* Input for tags */}
-    <input
-      type="text"
-      name="tags"
-      value={locationData.tags}
-      onChange={handleLocationInputChange}
-      placeholder="Tags (comma-separated)"
-    />
+          {/* Input for tags */}
+          <input
+            type="text"
+            name="tags"
+            value={locationData.tags}
+            onChange={handleLocationInputChange}
+            placeholder="Tags (comma-separated)"
+          />
 
-    <button type="submit">Add</button> 
-  </form>
-</Modal>
+          <button type="submit">Add</button>
+        </form>
+      </Modal>
 
+      {/* Modal for Deleting a Location */}
+      <Modal
+        isOpen={isDeletePlaceModalOpen}
+        onClose={() => setIsDeletePlaceModalOpen(false)}
+        title="Delete a Location"
+      >
+        <form onSubmit={handleDeleteLocation}>
+          <input
+            type="text"
+            value={placeIdToDelete}
+            onChange={(e) => setPlaceIdToDelete(e.target.value)}
+            placeholder="Location ID to delete"
+          />
+          <button type="submit">Delete Location</button>
+        </form>
+      </Modal>
+
+      <Modal
+        isOpen={isEditPlaceModalOpen}
+        onClose={() => setIsEditPlaceModalOpen(false)}
+        title="Edit a Place"
+      >
+        <form onSubmit={handleEditLocation}>
+          <input
+            type="number"
+            value={placeIdToEdit}
+            onChange={(e) => setPlaceIdToEdit(e.target.value)}
+            placeholder="Place ID to edit"
+          />
+
+          {/* Form fields for editing location attributes */}
+          <input
+            type="text"
+            name="title"
+            value={locationData.title}
+            onChange={handleLocationInputChange}
+            placeholder="Title"
+          />
+          <input
+            type="text"
+            name="description"
+            value={locationData.description}
+            onChange={handleLocationInputChange}
+            placeholder="Description"
+          />
+          <input
+            type="text"
+            name="image"
+            value={locationData.image}
+            onChange={handleLocationInputChange}
+            placeholder="Image URL"
+          />
+          <input
+            type="number"
+            name="estimated_price"
+            value={locationData.estimated_price}
+            onChange={handleLocationInputChange}
+            placeholder="Estimated Price"
+          />
+          <input
+            type="text"
+            name="area"
+            value={locationData.area}
+            onChange={handleLocationInputChange}
+            placeholder="Area"
+          />
+          <input
+            type="number"
+            name="rating"
+            value={locationData.rating}
+            onChange={handleLocationInputChange}
+            placeholder="Rating"
+          />
+          <input
+            type="number"
+            name="coordinates.latitude"
+            value={locationData.coordinates.latitude}
+            onChange={handleLocationInputChange}
+            placeholder="Latitude"
+          />
+          <input
+            type="number"
+            name="coordinates.longitude"
+            value={locationData.coordinates.longitude}
+            onChange={handleLocationInputChange}
+            placeholder="Longitude"
+          />
+          <input
+            type="number"
+            name="est_time_spend"
+            value={locationData.est_time_spend}
+            onChange={handleLocationInputChange}
+            placeholder="Estimated Time to Spend"
+          />
+          <input
+            type="text"
+            name="tags"
+            value={locationData.tags}
+            onChange={handleLocationInputChange}
+            placeholder="Tags (comma-separated)"
+          />
+          <select
+            name="category_ids"
+            value={locationData.category_ids}
+            onChange={handleLocationInputChange}
+            multiple
+          >
+            <option value="1">Beach</option>
+            <option value="2">Hiking</option>
+            <option value="3">Restaurants</option>
+            <option value="4">Ruins</option>
+          </select>
+
+          <button type="submit">Update Place</button>
+        </form>
+      </Modal>
     </div>
   );
 };
