902ab7da6e0bb38f23debd67822eddd9f26369dc
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-22 22:43:09 +0200

    [frontend][core]: implemented extra action and reducer for getting all users under test

--- frontend/core/Redux/Actions/authActions.js
+++ frontend/core/Redux/Actions/authActions.js
@@ -3,6 +3,7 @@ import { LOGIN_SUCCESS, LOGIN_FAILURE } from './actionTypes';
 import { REGISTER_SUCCESS, REGISTER_FAILURE } from './actionTypes';
 import  {LOGOUT, UPDATE_SUCCESS, UPDATE_FAILURE } from './actionTypes';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FETCH_USERS_FAILURE, FETCH_USERS_REQUEST , FETCH_USERS_SUCCESS } from './actionTypes';
 import { baseUrl } from '../../helpers/baseUrl';
 
 // Signup action
@@ -130,3 +131,19 @@ export const updateUser = (userId, userData) => {
     }
   };
 };
+
+export const fetchAllUsers = () => {
+return async (dispatch) => {
+  try {
+    dispatch({ type: FETCH_USERS_REQUEST});
+    const response = await axios.get(`${baseUrl}/getAllUsers`, { 
+      headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+  },});
+  dispatch({ type: FETCH_USERS_SUCCESS, payload: response.data });
+} catch (error) {
+  dispatch({ type: FETCH_USERS_FAILURE, payload: error.message || 'An unknown error occurred'});
+}
+};
+}
