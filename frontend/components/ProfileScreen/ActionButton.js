d1d1dbbe627b3d05c2e36c9c130a714492963715
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-23 04:25:49 +0200

    [frontend][styles]: implemented changes to the Done button to elevate and have transparent background

--- frontend/components/ProfileScreen/ActionButton.js
+++ frontend/components/ProfileScreen/ActionButton.js
@@ -3,31 +3,43 @@ import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 
 const ActionButton = ({ title, onPress }) => (
   <View style={styles.container}>
-  <TouchableOpacity style={styles.button} onPress={onPress}>
-    <Text style={styles.buttonText}>{title}</Text>
-  </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
   </View>
 );
 
-
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: 'transparent',
+    width: 350,
+    alignItems: 'center', 
+    position: 'absolute', 
+    bottom: 65,
   },
-    button: {
-      backgroundColor: '#A78BFA',
-      borderRadius: 25,
-      alignItems: 'center',
-      marginVertical: 2,
-      marginHorizontal: 25,
-      paddingVertical: 12,
-      paddingHorizontal: 40,
+  button: {
+    backgroundColor: 'transparent', 
+    backgroundColor: '#A78BFA', 
+    borderRadius: 25,
+    alignItems: 'center',
+    marginVertical: 2,
+    paddingHorizontal: 120,
+    paddingVertical: 12,
+    shadowColor: "#000", 
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    buttonText: {
-      color: 'white',
-      fontSize: 18,
-    },
-})
-
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText: {
+    color: '#fff', 
+    fontSize: 18,
+  },
+});
 
 export default ActionButton;
+
+
