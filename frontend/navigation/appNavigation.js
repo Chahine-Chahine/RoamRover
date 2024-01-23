524a6e2fa000df809fea0fa80c26d6e5ee9d4f8f
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-23 04:09:53 +0200

    [frontend][styles]: implemented changes in the header title for all stack screens

--- frontend/navigation/appNavigation.js
+++ frontend/navigation/appNavigation.js
@@ -32,16 +32,16 @@ export default AppNavigation = () => {
         <Stack.Screen name= 'Signup' options={{headerShown: false}} component={SignUpScreen}/>
         <Stack.Screen name= 'HomeScreen' options={{headerShown: false}} component={HomeScreen}/>
         <Stack.Screen name="TripDetailsScreen" component={TripDetailsScreen} options={{headerShown: true, headerTitle: 'Trip Details'}} />
-        <Stack.Screen name= 'LocationDetailScreen' options={{headerShown: true}} component={LocationDetailScreen}/>
+        <Stack.Screen name= 'LocationDetailScreen' options={{headerShown: true,  headerTitle:'Location Detail'}} component={LocationDetailScreen}/>
         <Stack.Screen name= 'profileScreen' options={{headerShown: true, headerTitle: 'Profile'}} component={ProfileScreen}/>
-        <Stack.Screen name= 'GenerateTrip' options={{headerShown: true}} component={GenerateTrip}/>
+        <Stack.Screen name= 'GenerateTrip' options={{headerShown: true, headerTitle:'Generate Trip'}} component={GenerateTrip}/>
         <Stack.Screen name= 'BookmarkScreen' options={{headerShown:true, headerTitle: 'Bookmarks'}} component={BookmarkScreen}/>
-        <Stack.Screen name= 'LoadingScreen' options={{headerShown:true}} component={LoadingScreen}/>
-        <Stack.Screen name= 'CustomtripScreen' options={{headerShown:true}} component={CustomtripScreen}/>
+        <Stack.Screen name= 'LoadingScreen' options={{headerShown:false}} component={LoadingScreen}/>
+        <Stack.Screen name= 'CustomtripScreen' options={{headerShown:true, headerTitle:'Custom Trip'}} component={CustomtripScreen}/>
         <Stack.Screen name='Questionnaire' options={{headerShown:true}} component={Questionnaire}/>
         <Stack.Screen name='ProfileUpdateScreen' options={{headerShown: true, headerTitle: 'Update Profile'}} component={ProfileUpdateScreen}/>
         <Stack.Screen name='ChatRoomScreen' options={{headerShown: false}} component={ChatRoomScreen}/>
-        <Stack.Screen name='RoomListScreen' options={{headerShown:true}} component={RoomListScreen}/>
+        <Stack.Screen name='RoomListScreen' options={{headerShown:true, headerTitle:'Room List'}} component={RoomListScreen}/>
         <Stack.Screen name='MapScreen' options={{headerShown: false}} component={MapScreen}/>
         </Stack.Navigator>  
     </NavigationContainer>
