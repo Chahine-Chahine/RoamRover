ac2ad70e1be1458d678c9a2d1fb6b08ce4b48bf2
Chahine-Chahine <chahinesquare@hotmail.com>
2024-01-24 21:28:38 +0200

    [backend][core]: added more seeders

--- backend/database/seeders/LocationsTableSeeder.php
+++ backend/database/seeders/LocationsTableSeeder.php
@@ -16,7 +16,7 @@ class LocationsTableSeeder extends Seeder
     {
         DB::table('locations')->insert([
             [
-                'title' => '3azme Caffe',
+                'title' => 'Azme Caffe',
                 'description' => 'Coffee shop in Beirut',
                 'estimated_price' => 20,
                 'area' => 'Beirut Lebanon',
@@ -27,13 +27,13 @@ class LocationsTableSeeder extends Seeder
                 "tags" => json_encode(["coffee", "snacks", "relaxation"])
             ],
             [
-                'title' => 'China City',
+                'title' => 'BHive',
                 'description' => 'New concept for studying in natural environment',
-                'estimated_price' => 20,
-                'area' => 'Ashrafieh Lebanon',
+                'estimated_price' => 25,
+                'area' => 'Hamra Lebanon',
                 'rating' => 4, 
-                'image' => "http://192.168.43.29:8000/storage/images/chinacity.png", 
-                'coordinates' => json_encode(['latitude' => 33.89550476190117, 'longitude' => 35.52042455421304]),
+                'image' => "http://192.168.43.29:8000/storage/images/beehive.webp", 
+                'coordinates' => json_encode(['latitude' => 33.895315062218195, 'longitude' => 35.47805602722215]),
                 "est_time_spend" => 180,
                 "tags" => json_encode(["study", "nature", "calm"])
             ],
@@ -46,8 +46,7 @@ class LocationsTableSeeder extends Seeder
                 'image' => "http://192.168.43.29:8000/storage/images/jeita-grotto.jpg",
                 'coordinates' => json_encode(['latitude' => 33.976480, 'longitude' => 35.621355]),
                 "est_time_spend" => 240,
-                "tags" => json_encode(["adventure", "nature
-                ", "exploration"])
+                "tags" => json_encode(["adventure", "nature", "exploration"])
             ],
             [
             'title' => 'Sidon Sea Castle',
@@ -60,6 +59,17 @@ class LocationsTableSeeder extends Seeder
             "est_time_spend" => 90,
             "tags" => json_encode(["history", "sightseeing", "photography"])
             ],
+            [
+                'title' => 'AquaLand resort',
+                'description' => 'Spend a day in aquaLand beach & resort, it is time to get tanned!',
+                'estimated_price' => 45,
+                'area' => 'Batroon Lebanon',
+                'rating' => 4,
+                'image' => "http://192.168.43.29:8000/storage/images/aqualand.jpg",
+                'coordinates' => json_encode(['latitude' => 34.249656130215186, 'longitude' => 35.658844202087145]),
+                "est_time_spend" => 120,
+                "tags" => json_encode(["Beach", "relaxing"])
+                ],
             [
             'title' => 'Byblos Old Port',
             'description' => 'Historic port in Byblos',
@@ -92,7 +102,40 @@ class LocationsTableSeeder extends Seeder
             'coordinates' => json_encode(['latitude' => 34.005434973434745 , 'longitude' => 36.21091110212627]),
             "est_time_spend" => 210,
             "tags" => json_encode(["cultural", "historic", "exploration"])
-            ]
+            ],
+            [
+                'title'=> 'Anjar Castle',
+                'area'=> 'Anjar Lebanon',
+                'description'=> 'Discover your history - explore beautiful Anjar castle today!',
+                'estimated_price'=> 15,
+                'rating' => 4,
+                'image'=> "http://192.168.43.29:8000/storage/images/anjar.jpg",
+                'coordinates' => json_encode(['latitude' => 33.732738726067836 , 'longitude' => 35.93361786954726]),
+                "est_time_spend" => 210,
+                "tags" => json_encode(["cultural", "historic", "exploration"])
+            ],
+            [
+                'title'=> 'Al Shouf Cedar Nature Reserve',
+                'area'=> 'Al Shouf Lebanon',
+                'description'=> 'Explore god wonders in Shouf what a journey!',
+                'estimated_price'=> 5,
+                'rating' => 5,
+                'image'=> "http://192.168.43.29:8000/storage/images/alshouf-cedar.jpg",
+                'coordinates' => json_encode(['latitude' => 33.7501118908014 , 'longitude' => 35.73154151187815]),
+                "est_time_spend" => 210,
+                "tags" => json_encode(["hiking", "nature", "scenery"])
+            ],
+            [
+                'title'=> 'Cedars of God',
+                'area'=> 'Bsharri Lebanon',
+                'description'=> 'The well known great Cedars of god is a great place to go for resetting your mind!',
+                'estimated_price'=> 0,
+                'rating' => 5,
+                'image'=> "http://192.168.43.29:8000/storage/images/cedars-of-god.jpg",
+                'coordinates' => json_encode(['latitude' => 34.244441479255684, 'longitude' => 36.04868297318802]),
+                "est_time_spend" => 180,
+                "tags" => json_encode(["hiking", "nature", "scenery"])
+                ]
             ]);
             $categoryIds = [
                 'Beach' => 1,
@@ -103,14 +146,18 @@ class LocationsTableSeeder extends Seeder
     
             // Manually assign categories to each location.
             $locations = [
-                '3azme Caffe' => ['Restaurants'],
-                'China City' => ['Restaurants', 'Hiking'],
+                'Azme Caffe' => ['Restaurants'],
+                'BHive' => ['Restaurants'],
                 'Jeita Grotto' => ['Hiking'],
                 'Byblos Old Port' => ['Beach'],
                 'Qadisha Valley' => ['Hiking'],
                 'Baalbeck Castle' => ['Ruins'],
                 'Sidon Sea Castle' => ['Beach'],
-                
+                'Anjar Castle' => ['Ruins'],
+                'Cedars of God' => ['Hiking'],
+                'AquaLand resort' => ["Beach"],
+                'Al Shouf Cedar Nature Reserve' => ['Hiking'],
+
             ];
     
             foreach ($locations as $title => $cats) {
